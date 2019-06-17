42df04b1f334023e0375ca89ac8661a4978c6af7
mike1402b <mike1402@gmx.at>
2019-06-17 13:49:44 +0200

    diverse infos hinzu

--- gpio.php
+++ gpio.php
@@ -1,18 +1,16 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width" />
-<title>Schalter</title>
+<title>gpio</title>
 </head>
 <body>
   <h1>gpio read</h1>
   <p>version vom 20190617 1314</P>
-  <?php 
-  echo "test" 
-  ?>
+  <p>relay z.b. pumpe genau invers 0= ein, 1= aus </p>
   <table>
-  <tr><td>18</td><td><?php echo exec("gpio -g read 18");?></td></tr>
-  <tr><td>23</td><td><?php echo exec("gpio -g read 23");?></td></tr>
-  <tr><td>24</td><td><?php echo exec("gpio -g read 24");?></td></tr>
-  <tr><td>25</td><td><?php echo exec("gpio -g read 25");?></td></tr>
+    <tr><td>18 =</td><td><?php echo exec("gpio -g read 18");?></td></tr>
+    <tr><td>23 pumpe =</td><td><?php echo exec("gpio -g read 23");?></td></tr>
+    <tr><td>24 =</td><td><?php echo exec("gpio -g read 24");?></td></tr>
+    <tr><td>25 =</td><td><?php echo exec("gpio -g read 25");?></td></tr>
   </table>
 </body>
